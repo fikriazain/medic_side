1c16a79111280238f3e885d5b103c4e049d8dd3e
Fikri Aufaa Zain <fikri.azain@gmail.com>
2024-06-05 20:38:16 +0700

    add first message

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, send_message  # Import the view for chatting with LLM
+from .views import index, send_message, first_message  # Import the view for chatting with LLM
 
 urlpatterns = [
     # Direct to main page in templates/index.html
     path('', index, name='index_message'),
     path('send_message/', send_message, name='send_message'),  # Add the URL for chatting with LLM
+    path('first_message/', first_message, name='first_message'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -4,10 +4,24 @@ from django.http import JsonResponse
 import requests
 from django.views.decorators.csrf import csrf_exempt
 from deep_translator import GoogleTranslator
+import datetime
+import zoneinfo
 
 
 
 # Create your views here.
+@csrf_exempt
+def first_message(request):
+    jakarta_tz = zoneinfo.ZoneInfo('Asia/Jakarta')
+    jakarta_time = datetime.datetime.now(jakarta_tz)
+    data ={
+        'username': request.session.get('user_id'),
+        "current_date_time": jakarta_time.strftime("%Y-%m-%d %I:%M:%S %p %A"),
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post('http://127.0.0.1:5006/first_message', json=data, headers=headers)
+    return JsonResponse(response.json())
+
 @csrf_exempt
 def index(request):
     return render(request, 'index_chat.html')
@@ -26,9 +40,10 @@ def get_llm_response(text, username):
     data = {'input': text, 'username': username}
     headers = {'Content-Type': 'application/json'}
     try:
-        response = requests.post('http://127.0.0.1:5005/query', json=data, headers=headers)
+        print("Fetching LLM response...")
+        response = requests.post('http://127.0.0.1:5006/query', json=data, headers=headers)
         response_json = response.json()
-        return response_json
+        return response_json            
     except Exception as e:
         print(f"Error fetching LLM response: {e}")
         return "Error: Failed to fetch response from LLM server"
