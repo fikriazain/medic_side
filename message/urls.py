cf356dffa7de422a486618d7f027126214afa2fc
Adhin Abdallah <adhinab25@gmail.com>
2024-06-09 21:36:18 +0700

    fix ui

--- message/urls.py
+++ message/urls.py
@@ -5,7 +5,7 @@ from .views import index, get_message, delete_all_data
 
 urlpatterns = [
     #Direct to main page in tempaltes/index.html
-    path('', index, name='index_message'),
+    path('', index, name='message_list'),
     path('get_message/', get_message, name='get_message'),
     path('delete/', delete_all_data, name='delete_all_data')
 
